8877942adee4890b56602d89537efa8ab9c7b1fa
Christopher Hu <40853270+christopher192@users.noreply.github.com>
2023-09-29 01:15:43 +0800

    update

--- helper_function.py
+++ helper_function.py
@@ -62,7 +62,7 @@ def preprocess_text_with_line_number(file_name):
   
     return abstract_sample
 
-def plot_loss_curves(history): 
+def plot_loss_curve(history): 
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
@@ -94,3 +94,35 @@ def unzip_data(file_name):
 def walk_through_dir(dir_path):
     for dirpath, dirnames, filenames in os.walk(dir_path):
         print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+def compare_history(original_history, new_history, initial_epochs = 5):
+    acc = original_history.history["accuracy"]
+    loss = original_history.history["loss"]
+
+    val_acc = original_history.history["val_accuracy"]
+    val_loss = original_history.history["val_loss"]
+
+    total_acc = acc + new_history.history["accuracy"]
+    total_loss = loss + new_history.history["loss"]
+
+    total_val_acc = val_acc + new_history.history["val_accuracy"]
+    total_val_loss = val_loss + new_history.history["val_loss"]
+
+    plt.figure(figsize = (8, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label = 'Training Accuracy')
+    plt.plot(total_val_acc, label = 'Validation Accuracy')
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+        plt.ylim(), label = 'Start Fine Tuning')
+    plt.legend(loc = 'lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label = 'Training Loss')
+    plt.plot(total_val_loss, label = 'Validation Loss')
+    plt.plot([initial_epochs - 1, initial_epochs - 1],
+        plt.ylim(), label = 'Start Fine Tuning')
+    plt.legend(loc = 'upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
